9c238a90cdd56430d090fa30a50663a008adbf30
unknown <v.hoogelander@student.tudelft.nl>
2022-11-23 10:05:47 +0100

    finalize model

--- HBVmountain/General/Calibration.py
+++ HBVmountain/General/Calibration.py
@@ -49,23 +49,20 @@ def multi_objective(Qmodelled, Qobserved, Precipitation):
 
     return ED, nse, lognse, nseFDC, nse_runoff
 
-POPSIZE = 9
-MAXITER = 50
-####### Forcing and observation data ##########################
-def run_model_cma(parameters):
+def run_model_cma(parameters, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, end_year):
 
     ob_list = []
     params_list = []
     
 
-    forcing = nc.Dataset('Data/ThunderCreek/HBVmountain_ERA5_ThunderCreek_1986_2005.nc') #Catchment dependent
-    model = BMI_HBVmountain(forcing_netcdf=forcing)
+   
+    model = BMI_HBVmountain(forcing, path_to_shapefile, path_to_dem, path_to_nlcd)
     config_file = model.setup(forcing_netcdf=forcing, bare_parameters=  Parameters(parameters[8], parameters[6], 0, 0, parameters[4],
                                                                      parameters[1], parameters[2], parameters[3], parameters[7], parameters[0]),
                                         forest_parameters=Parameters(parameters[11], parameters[6], 0, parameters[9], parameters[4],
                                                                      parameters[1], parameters[2], parameters[3], parameters[10], parameters[0]),
                                         grass_parameters= Parameters(parameters[14], parameters[6], 0, parameters[12], parameters[4],
-                                                                     parameters[1], pavirameters[2], parameters[3], parameters[13], parameters[0]),
+                                                                     parameters[1], parameters[2], parameters[3], parameters[13], parameters[0]),
                                         rip_parameters=   Parameters(parameters[17], parameters[6], 0, parameters[15], parameters[18],
                                                                      parameters[1], parameters[2], parameters[3], parameters[16], parameters[0]),
                                         slow_parameters=  Slow_Paramters(parameters[5], parameters[19]))
@@ -73,23 +70,10 @@ def run_model_cma(parameters):
 
 
 
-########### Catchment dependent settings ######################
-
-    model.set_value('Elevation', Main.Elevations(587.5, 396.0, 2746.0, 1572.36, 1572.36))
-    model.set_value('Glacier', [0.0, 0.066, 0.7, 0.234])
-    model.set_value('Sunhours', [8.87, 10.30, 11.88, 13.65, 15.13, 15.97, 15.58, 14.25, 12.62, 11.87, 9.28, 8.45]) #Seattle
-    model.set_value('bare_input', Main.HRU_Input([0.001, 0.083, 0.636, 0.28], 0.32, [0.0, 0.021, 0.222, 0.074], [1,2,3,4], 4, (0,), (0,), 0, np.zeros(4), 0.01, np.zeros(4), 0, 0.0))
-    model.set_value('forest_input', Main.HRU_Input([0.216, 0.396, 0.368, 0.02], 0.46,np.zeros(4), [1,2,3,4], 4, (0,), (0,), 0, np.zeros(4), 0.01, np.zeros(4), 0, 0.0))
-    model.set_value('grass_input', Main.HRU_Input([0.077, 0.263, 0.546, 0.114], 0.21,np.zeros(4), [1,2,3,4], 4, (0,), (0,), 0, np.zeros(4), 0.01, np.zeros(4), 0, 0.0))
-    model.set_value('rip_input', Main.HRU_Input([0.65, 0.096, 0.232, 0.022], 0.01,np.zeros(4), [1,2,3,4], 4, (0,), (0,), 0, np.zeros(4), 0.01, np.zeros(4), 0, 0.0))
-    model.set_value('Total_Elevationbands', 4)
-    model.set_value('Elevation_Percentage', [0.15, 0.312, 0.416, 0.122])
- 
-
 
     Discharge = []
     timestamp = []
-    while (model.get_value_ptr('Current_Date') < (datetime.date(1998, 12, 31))):  
+    while (model.get_value_ptr('Current_Date') < (datetime.date(end_year, 12, 31))):  
         model.update()
         timestamp.append(model.get_value_ptr('Current_Date'))
         Discharge.append(model.get_value_ptr('Discharge'))
@@ -143,25 +127,26 @@ def transform(scaled_parameters):
         parameters.append(parameter)
     return parameters
 
-def wrapped_objective_function(scaled_parameters, observation, iterations):
+def wrapped_objective_function(scaled_parameters, observation, forcing, path_to_shapefile, path_to_dem, path_to_nlcd,start_year, years_warming_up, end_year):
     """A wrapper around the objective function.
 
     The wrapper transforms the scaled_parameters before
     the actual function call.
     """
     parameters = transform(scaled_parameters)
-    Qmodelled, precipitation = run_model_cma(parameters)
+    Qmodelled, precipitation = run_model_cma(parameters, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, end_year)
     precipitation.index, Qmodelled.index, observation.index = pd.to_datetime(precipitation.index), pd.to_datetime(Qmodelled.index), pd.to_datetime(observation.index)
     mask = (observation.index >= Qmodelled.index[0]) & (observation.index <= Qmodelled.index[-1])
     observation = observation.loc[mask]
 
-    ED = multi_objective(Qmodelled.loc[Qmodelled.index.year >= 1989], observation.loc[observation.index.year >= 1989], precipitation)[0] 
+    ED = multi_objective(Qmodelled.loc[Qmodelled.index.year >= (start_year+years_warming_up)], observation.loc[observation.index.year >= (start_year+years_warming_up)], precipitation)[0] 
     return ED
 
-def cma_calibration(parameter_bounds, observation, MAXITER):
+def cma_calibration(parameter_bounds, observation, MAXITER, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, start_year, years_warming_up, end_year):
     """Return the optimum parameters found by CMA-ES method."""
     # Set some options for optimization needed when multiprocessing
     options = cma.CMAOptions()
+    POPSIZE = 9
     options.set({
         'bounds': [0, 10],  # for scaled parameters
 #         'seed': 1234,  # set a seed to reproduce results
@@ -193,7 +178,7 @@ def cma_calibration(parameter_bounds, observation, MAXITER):
         cma_es.tell(
             solutions,
 
-            [wrapped_objective_function(x, observation) for x in solutions]
+            [wrapped_objective_function(x, observation, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, start_year, years_warming_up, end_year) for x in solutions]
                 #function_values=wrapped_objective_function(solutions, observation, area)
             )#,
 
@@ -210,7 +195,8 @@ def cma_calibration(parameter_bounds, observation, MAXITER):
     return full_output
 
 
-def full_calibration(ntimes, iterations):
+def full_calibration(ntimes, observation, MAXITER, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, start_year, years_warming_up, end_year):
+    exec_start_time = time.time()
     PARAMETERS_BOUNDS = [[-2.0, 2.0],
                      [1.0, 5.0],
                      [0.001, 1.0],
@@ -231,20 +217,20 @@ def full_calibration(ntimes, iterations):
                      [0.1, 2.0],
                      [0.1, 3.0],
                      [0.05, 0.5]]
-    area = 271.9 #km2
-    observation = pd.read_csv('Data/ThunderCreek/Discharge_ThunderCreek.csv', index_col=0).streamflow / (area * 1e6) * 1000 *86400
+    area = gpd.read_file(path_to_shapefile).area_hys.values[0] #km2
+    observation = pd.read_csv(observation, index_col=0).streamflow / (area * 1e6) * 1000 *86400
     ob_list = []
     params_list = []
     results_doc_list = []
     for n in range(ntimes):
-        full_output = cma_calibration(PARAMETERS_BOUNDS, observation, iterations)
+        full_output = cma_calibration(PARAMETERS_BOUNDS, observation, MAXITER, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, start_year, years_warming_up, end_year)
         best_par, best_score, pars, scores = full_output
         
-        simulated_discharge, precipitation = run_model_cma(best_par)
+        simulated_discharge, precipitation = run_model_cma(best_par, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, end_year)
         mask = (observation.index >= simulated_discharge.index[0]) & (observation.index <= simulated_discharge.index[-1])
         observation = observation.loc[mask]
         
-        objective_function = multi_objective(simulated_discharge.loc[simulated_discharge.index.year >= 1989], observation.loc[observation.index.year >= 1989], precipitation)
+        objective_function = multi_objective(simulated_discharge.loc[simulated_discharge.index.year >= (start_year+ years_warming_up)], observation.loc[observation.index.year >= (start_year+ years_warming_up)], precipitation)
         ob_list.append(objective_function)
         params_list.append(best_par)   
         
@@ -264,16 +250,18 @@ def full_calibration(ntimes, iterations):
         results_doc_list.append(results_doc)
     paramset = pd.DataFrame(columns=['ED', 'NSE', 'logNSE', 'NSEfdc', 'NSErunoff', 'Temp_Thresh', 'Meltfactor', 'Mm', 'Ratio_Pref', 'Kf', 'Ks', 'Ce', 'Soilstoragecapacity_Bare', 'beta_Bare', 'Interceptioncapacity_Forest', 'Soilstoragecapacity_Forest', 'beta_Forest', 'Interceptioncapacity_Grass', 'Soilstoragecapacity_Grass', 'beta_Grass', 'Interceptioncapacity_Rip', 'Soilstoragecapacity_Rip', 'beta_Rip', 'Kf_Rip', 'Ratio_Riparian'])
     for i in range(len(ob_list)):
-        print(ob_list)
         paramset.loc[i] = [ob_list[i][0], ob_list[i][1], ob_list[i][2], ob_list[i][3], ob_list[i][4], params_list[i][0], params_list[i][1], params_list[i][2], params_list[i][3], params_list[i][4], params_list[i][5], params_list[i][6], params_list[i][7], params_list[i][8], params_list[i][9], params_list[i][10], params_list[i][11], params_list[i][12], params_list[i][13], params_list[i][14], params_list[i][15], params_list[i][16], params_list[i][17], params_list[i][18], params_list[i][19]]
-
+    paramset.ED = 1 - paramset.ED
     filename = 'cma_calibration_thundercreek.yml'
     yaml = YAML()
     yaml.default_flow_style = False
     with open(filename, 'w+') as f:
         yaml.dump(results_doc_list, f)
     print(f'Calibration results saved in {filename}')
+    print(f"Calibration is completed in {time.time() - exec_start_time} seconds")
     return paramset
+
+
 
 
 def main():
@@ -292,13 +280,13 @@ def main():
 
 
     partition_item = comm.scatter(partitions, root=0)
-    paramset_item = full_calibration(partition_item)
+    paramset_item = full_calibration(partition_item,  observation, MAXITER, forcing, path_to_shapefile, path_to_dem, path_to_nlcd, start_year, years_warming_up, end_year)
     paramset = comm.gather(paramset_item, root=0)
 
     if rank == 0:
         paramset = pd.concat(paramset)
         name = 'paramsets_thundercreek.csv'
-        outdir = './output_tc'
+        outdir = './output'
         if not os.path.exists(outdir):
             os.mkdir(outdir)
         fullname = os.path.join(outdir, name)
